ef7c2cff4be25f5b437ac9c80b9dc61109cee89b
okbalogs <okbalogs@gmail.com>
2026-01-15 21:04:48 +0100

    Initial Commit of Python

---
+++ day1_basics.py
@@ -0,0 +1,10 @@
+my_career_path = ["Backend", "System", "AI"]
+print ("Starting my journey at UNILAG...")
+
+for path in my_career_path:
+	print("In 2028, i will be a specilist in: " + path)
+
+a = 256
+b = 256
+print("memory check - are they the same object")
+print(a is b)
